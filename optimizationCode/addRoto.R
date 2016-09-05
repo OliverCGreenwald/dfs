7ba2c0fbd1ef519c4878b15c077886b7b5915c7e
Alan <alan.du20@gmail.com>
2016-09-05 16:13:29 -0400

    replace DK projections with Roto
    also some csv re-organizing
    defense.csv not changed at the moment (team names don’t match)
    still need to remove “Jr.” and “Sr.” from DK

--- optimizationCode/addRoto.R
+++ optimizationCode/addRoto.R
@@ -1,27 +1,43 @@
-setwd("data_warehouse")
+#setwd("data_warehouse")
+#setwd("~/DFS/optimizationCode")
+setwd("~/Documents/PrincetonFall16/fantasyfootball/DFS/optimizationCode")
+
 
 #--------- offense ---------#
 # read in data
-dk.offense.data <- read.csv("offensive_players.csv", header = T, stringsAsFactors = F)
-roto.offense.data <- read.csv("roto_offense.csv", header = T, stringsAsFactors = F)
+dk.offense.data <- read.csv("data_warehouse/dk_offensive_players.csv", header = T, stringsAsFactors = F)
+roto.offense.data <- read.csv("data_warehouse/roto_offense.csv", header = T, stringsAsFactors = F)
 
 # compare dk and roto data
 nrow(dk.offense.data[dk.offense.data$Projection>0,])
 nrow(roto.offense.data[roto.offense.data$fpts>0,])
 
 # split roto names into first and last name columns
-library(stringr)
-first.last.name <- str_split_fixed(roto.offense.data$player, " ", 2)
-colnames(first.last.name) <- c("FirstName", "LastName")
-roto.offense.data <- cbind(first.last.name, roto.offense.data[,2:ncol(roto.offense.data)])
+#library(stringr)
+#first.last.name <- str_split_fixed(roto.offense.data$player, " ", 2)
+#colnames(first.last.name) <- c("FirstName", "LastName")
+#roto.offense.data <- cbind(first.last.name, roto.offense.data[,2:ncol(roto.offense.data)])
 
-#
-sum(roto.offense.data$FirstName %in% dk.offense.data$FirstName == F) # number of players in roto but not in dk
-#dk.offense.data[which(dk.offense.data$FirstName %in% roto.offense.data$FirstName), 'Projection'] <- roto.offense.data[which(roto.offense.data$FirstName %in% dk.offense.data$FirstName), 'fpts']
+# combine dk first and last names
+dk.offense.data$FullName <- paste(dk.offense.data$FirstName, dk.offense.data$LastName)
+
+# replace dk projections with roto projections
+dk.offense.data$RotoProjection <- roto.offense.data$fpts[match(dk.offense.data$FullName, roto.offense.data$player)]
+dk.offense.data$RotoProjection[is.na(dk.offense.data$RotoProjection)] <- 0
+dk.offense.data$Projection <- dk.offense.data$RotoProjection
+dk.offense.data$RotoProjection <- NULL
+dk.offense.data$FullName <- NULL
+
+# write to file
+write.csv(dk.offense.data, file = 'data_warehouse/offensive_players.csv', row.names = F)
 
 
 #--------- defense ---------#
 # read in data
-defense.data <- read.csv("defenses.csv", header = T, stringsAsFactors = F)
-roto.defense.data <- read.csv("roto_defense.csv", header = T, stringsAsFactors = F)
+dk.defense.data <- read.csv("data_warehouse/dk_defenses.csv", header = T, stringsAsFactors = F)
+roto.defense.data <- read.csv("data_warehouse/roto_defense.csv", header = T, stringsAsFactors = F)
 
+# team names are in different formats
+
+# write to file
+write.csv(dk.defense.data, file = 'data_warehouse/defenses.csv', row.names = F)
