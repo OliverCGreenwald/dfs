7fd44ae71aaeb9f093dfc6a3c9cc4909f582b580
Alan <alan.du20@gmail.com>
2016-09-16 15:55:34 -0400

    cleaning

--- optimizationCode/calculateExposure.R
+++ optimizationCode/calculateExposure.R
@@ -1,6 +1,7 @@
 setwd("~/Projects/DFS/optimizationCode")
+#setwd("~/Documents/PrincetonFall16/fantasyfootball/DFS/optimizationCode")
 
-data <- read.csv("output.csv", stringsAsFactors = F, header = F)
+data <- read.csv("output_new.csv", stringsAsFactors = F, header = F)
 
 occurences<-sort(table(unlist(data)), decreasing=T)
 
--- testingLineups/Analyze_Output_Results.R
+++ testingLineups/Analyze_Output_Results.R
@@ -1,16 +1,23 @@
 setwd("~/Projects/DFS/testingLineups")
+setwd("/Users/Alan/Documents/PrincetonFall16/fantasyfootball/DFS/testingLineups/")
+#load("RData_files/output_2015_week1_2overlap.RData")
+#load("RData_files/output_2015_week1_3overlap.RData")
+#load("RData_files/output_2015_week1_4overlap.RData")
+#load("RData_files/output_2015_week1_5overlap.RData")
 
 data <- read.csv("output.csv", stringsAsFactors = F, header = F)
 results_offense <- read.csv("data_warehouse/offensive_players.csv", stringsAsFactors = T, header = T)
 results_defense <- read.csv("data_warehouse/defenses.csv", stringsAsFactors = T, header = T)
+#plot(results_offense$Projection, results_offense$ProductionPoints, xlim=c(0, 35))
+#View(results_offense[order(results_offense$ProductionPoints),])
 occurences<-sort(table(unlist(data)), decreasing=T)
 
 exposure<- occurences / nrow(data)
 exposure
 
 results_offense$Name <- paste(results_offense$FirstName, results_offense$LastName, sep = " ")
-results_offense <- results_offense[,c('Name', 'ProductionPoints')]
-results_defense <- results_defense[,c('Name', 'ProductionPoints')]
+results_offense <- results_offense[,c('Name', 'Projection')]
+results_defense <- results_defense[,c('Name', 'Projection')]
 
 total_results <- rbind(results_defense, results_offense)
 data$total <- 0
@@ -19,5 +26,8 @@ for (index in 1:nrow(data)){
   row <- t(data[index,])
   colnames(row) <- 'Name'
   row <- merge(row, total_results, by = 'Name')
-  data$total[index] <- sum(row$ProductionPoints)
+  data$total[index] <- sum(row$Projection)
 }
+
+sort(data$total) # production points of the lineups in ascending order
+View(data[order(data$total),])
