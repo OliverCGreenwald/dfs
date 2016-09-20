b619b98032467acea87952efd152a0c4522bfae6
Michael Chiang <mlchiang@princeton.edu>
2016-09-19 22:37:56 -0400

    moving things around, and updating analysis of results

--- resultsAnalysis/analyzeResults.R
+++ resultsAnalysis/analyzeResults.R
@@ -1,17 +1,33 @@
-setwd("~/Documents/PrincetonFall16/fantasyfootball/DFS/resultsAnalysis")
+#setwd("~/Documents/PrincetonFall16/fantasyfootball/DFS/resultsAnalysis")
 
 data.week1 <- read.csv("data_warehouse/draftkings_results_week1.csv", stringsAsFactors = F)
 data.week2 <- read.csv("data_warehouse/draftkings_results_week2.csv", stringsAsFactors = F)
+
+max.points.week1 <- 250.52
+min.winner.week1 <- 135
+min.percentile.winner.week1 <- 0.63
+
+max.points.week2 <- 238.12
+min.winner.week2 <- 143
+min.percentile.winner.week2 <- 0.73
 
 data.week1$Percentile_Place <- 1-data.week1$Place/data.week1$Contest_Entries
 data.week2$Percentile_Place <- 1-data.week2$Place/data.week2$Contest_Entries
 
 par(mfrow=c(1,2))
-hist(data.week1$Percentile, ylim=c(0,35))
-hist(data.week2$Percentile, ylim=c(0,35))
+hist(data.week1$Percentile_Place, ylim=c(0,35))
+abline(v=min.percentile.winner.week1, col = 'red')
+hist(data.week2$Percentile_Place, ylim=c(0,35))
+abline(v=min.percentile.winner.week2, col = 'red')
 
-max.points.week2 <- 250.52
-max.points.week2 <- 238.12
+data.week1$Percentile_Points <- data.week1$Points/max.points.week1
+data.week2$Percentile_Points <- data.week2$Points/max.points.week2
 
-data.week1$Percentile_Points <- 1-data.week1$Points/
-data.week2$Percentile_Points <- 1-data.week2$Points/num.entries.week2
+par(mfrow=c(1,2))
+hist(data.week1$Percentile_Points, ylim=c(0,35), xlim=c(0,1))
+abline(v=min.winner.week1/max.points.week1, col = 'red')
+hist(data.week2$Percentile_Points, ylim=c(0,35), xlim=c(0,1))
+abline(v=min.winner.week2/max.points.week2, col = 'red')
+
+plot(data.week1$Percentile_Place, data.week1$Percentile_Points)
+plot(data.week2$Percentile_Place, data.week2$Percentile_Points)
