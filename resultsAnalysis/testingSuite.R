5e9e9d146c358a9449c31d1939011beeec9c50da
Alan <alan.du20@gmail.com>
2016-09-27 12:21:13 -0400

    update testing suite

--- resultsAnalysis/testingSuite.R
+++ resultsAnalysis/testingSuite.R
@@ -1,30 +1,21 @@
 #setwd("~/Projects/DFS/resultsAnalysis")
+#setwd("~/Documents/PrincetonFall16/fantasyfootball/DFS/resultsAnalysis")
 
-#--------- Set week number automation purposes ---------#
-week.num <- ceiling((as.numeric(Sys.Date()) - as.numeric(as.Date("2016-09-11")))/7 + 1)
+#--------- Set week number for testing ---------#
+week.num <- 2 # change this!
 
-#--------- Input DF's ---------#
-contest.results.week3 <- read.csv("data_warehouse/contest_lineups_week3.csv", stringsAsFactors = F)
-player.performance.week1 <- read.csv("data_warehouse/player_weekly_performance/draftkings_player_production_week1.csv", stringsAsFactors = F)
-player.performance.week2 <- read.csv("data_warehouse/player_weekly_performance/draftkings_player_production_week2.csv", stringsAsFactors = F)
-player.performance.week3 <- read.csv("data_warehouse/player_weekly_performance/draftkings_player_production_week3.csv", stringsAsFactors = F)
+####### IMPORT AND CLEAN DK HISTORICAL FPTS DATA FOR THE WEEK #########
+file.name <- paste0("data_warehouse/player_weekly_performance/draftkings_player_production_week", week.num, ".csv")
+player.performance <- read.csv(file = file.name, stringsAsFactors = F)
 
+player.performance$Player <- sub(' Sr.','', player.performance$Player)
+player.performance$Player <- sub(' Jr.','', player.performance$Player)
 
-####### CLEAN PLAYER PERFORMANCE CSV'S #########
-player.performance.week1$Player <- sub(' Sr.','', player.performance.week1$Player)
-player.performance.week1$Player <- sub(' Jr.','', player.performance.week1$Player)
+######## IMPORT SUBMITTED LINEUPS ########
+file.name <- paste0("../optimizationCode/submitted_lineups/week", week.num, "_lineups.csv")
+lineups <- read.csv(file = file.name, stringsAsFactors = F)
 
-player.performance.week2$Player <- sub(' Sr.','', player.performance.week2$Player)
-player.performance.week2$Player <- sub(' Jr.','', player.performance.week2$Player)
-
-player.performance.week3$Player <- sub(' Sr.','', player.performance.week3$Player)
-player.performance.week3$Player <- sub(' Jr.','', player.performance.week3$Player)
-
-######## CHANGE THIS TO YOUR FILE THAT YOU WANT TO COMPARE TO: ########
-
-lineups <- read.csv("../optimizationCode/submitted_lineups/week1_lineups.csv", stringsAsFactors = F)
-
-######## Clean Output ########
+######## CALCULATE FPTS FOR EACH LINEUP ########
 for (i in 1:ncol(lineups)) {
   lineups[,i] <- substr(lineups[,i], 1, regexpr('\\(', lineups[,i]) - 2)
   lineups[,i] <- sub(' Sr.','', lineups[,i])
@@ -32,7 +23,7 @@ for (i in 1:ncol(lineups)) {
 }
 lineups[,ncol(lineups)] <- substr(lineups[,ncol(lineups)], 1, nchar(lineups[,ncol(lineups)])-1)
 
-total_results <- player.performance.week1[,c('Player', 'Actual.Score')]
+total_results <- player.performance[,c('Player', 'Actual.Score')]
 lineups$total <- 0
 
 for (index in 1:nrow(lineups)){
@@ -42,4 +33,4 @@ for (index in 1:nrow(lineups)){
   lineups$total[index] <- sum(row$Actual.Score)
 }
 
-plot(lineups$total, main = 'week 1')
+plot(lineups$total, main = paste0("Week ", week.num), ylab = "Lineup FPts")
