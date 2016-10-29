eee9c1a68ff2655d27474744ac3ba323168fdf00
Alan <alan.du20@gmail.com>
2016-10-29 13:44:07 -0400

    cleaning

--- projectionsAnalysis/perturbProjections.R
+++ projectionsAnalysis/perturbProjections.R
@@ -7,75 +7,40 @@
 # mean 0 and variance "projection error".
 
 
-###### COMPUTE PROJECTION ERROR (ROTOGRINDERS AND DFN) FOR EACH PLAYER FOR EACH WEEK #########
+###### COMPUTE PROJECTION ERROR (DFN) FOR EACH PLAYER #########
 
-####### Week 1 #########
-fpts.realized.week1 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week1.csv', stringsAsFactors = F)
+addError <- function(realized.data, projections.data) {
+  projections.data$Actual_fpts <- realized.data$Actual.Score[match(projections.data$Name, realized.data$Player)]
+  projections.data$Error_fpts_dfn <- abs(projections.data$Projection_dfn - projections.data$Actual_fpts)
+  return(projections.data)
+}
 
-fpts.offense.week1 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk1/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week1$Actual_fpts <- fpts.realized.week1$Actual.Score[match(fpts.offense.week1$Name, fpts.realized.week1$Player)]
-fpts.offense.week1$Error_fpts_dfn <- abs(fpts.offense.week1$Projection_dfn - fpts.offense.week1$Actual_fpts)
-
-####### Week 2 #########
-fpts.realized.week2 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week2.csv', stringsAsFactors = F)
-
-fpts.offense.week2 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk2/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week2$Actual_fpts <- fpts.realized.week2$Actual.Score[match(fpts.offense.week2$Name, fpts.realized.week2$Player)]
-fpts.offense.week2$Error_fpts_dfn <- abs(fpts.offense.week2$Projection_dfn - fpts.offense.week2$Actual_fpts)
-
-# fpts.offense.week2$Name <- sub(' Sr.', '', fpts.offense.week2$Name) # remove Sr.
-# fpts.offense.week2$Name <- sub(' Jr.', '', fpts.offense.week2$Name) # remove Jr.
-
-####### Week 3 #########
-fpts.realized.week3 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week3.csv', stringsAsFactors = F)
-
-fpts.offense.week3 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk3/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week3$Actual_fpts <- fpts.realized.week3$Actual.Score[match(fpts.offense.week3$Name, fpts.realized.week3$Player)]
-fpts.offense.week3$Error_fpts_dfn <- abs(fpts.offense.week3$Projection_dfn - fpts.offense.week3$Actual_fpts)
-
-# fpts.offense.week3$Name <- sub(' Sr.', '', fpts.offense.week3$Name) # remove Sr.
-# fpts.offense.week3$Name <- sub(' Jr.', '', fpts.offense.week3$Name) # remove Jr.
-
-####### Week 4 #########
-fpts.realized.week4 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week4.csv', stringsAsFactors = F)
-
-fpts.offense.week4 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk4/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week4$Actual_fpts <- fpts.realized.week4$Actual.Score[match(fpts.offense.week4$Name, fpts.realized.week4$Player)]
-fpts.offense.week4$Error_fpts_dfn <- abs(fpts.offense.week4$Projection_dfn - fpts.offense.week4$Actual_fpts)
-
-# fpts.offense.week4$Name <- sub(' Sr.', '', fpts.offense.week4$Name) # remove Sr.
-# fpts.offense.week4$Name <- sub(' Jr.', '', fpts.offense.week4$Name) # remove Jr.
-
-####### Week 5 #########
-fpts.realized.week5 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week5.csv', stringsAsFactors = F)
-
-fpts.offense.week5 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk5/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week5$Actual_fpts <- fpts.realized.week5$Actual.Score[match(fpts.offense.week5$Name, fpts.realized.week5$Player)]
-fpts.offense.week5$Error_fpts_dfn <- abs(fpts.offense.week5$Projection_dfn - fpts.offense.week5$Actual_fpts)
-
-# fpts.offense.week5$Name <- sub(' Sr.', '', fpts.offense.week5$Name) # remove Sr.
-# fpts.offense.week5$Name <- sub(' Jr.', '', fpts.offense.week5$Name) # remove Jr.
+week.latest <- ceiling((as.numeric(Sys.Date()) - as.numeric(as.Date("2016-09-11")))/7 + 1) - 1
+for (i in 1:week.latest) {
+  name.realized <- paste("fpts.realized.week", i, sep = "")
+  assign(name.realized, read.csv(file = paste0('resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week', i, '.csv'), stringsAsFactors = F))
+  
+  name.projections <- paste("fpts.offense.week", i, sep = "")
+  assign(name.projections, read.csv(file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/wk', i, '/offensive_players.csv'), stringsAsFactors = F))
+  
+  assign(name.projections, addError(eval(parse(text=name.realized)), eval(parse(text=name.projections))))
+}
 
 
-####### Week 6 #########
-fpts.realized.week6 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week6.csv', stringsAsFactors = F)
+#----- any code below this line needs to be updated and/or cleaned -----#
 
-fpts.offense.week6 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk6/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week6$Actual_fpts <- fpts.realized.week6$Actual.Score[match(fpts.offense.week6$Name, fpts.realized.week6$Player)]
-fpts.offense.week6$Error_fpts_dfn <- abs(fpts.offense.week6$Projection_dfn - fpts.offense.week6$Actual_fpts)
-
-# fpts.offense.week6$Name <- sub(' Sr.', '', fpts.offense.week6$Name) # remove Sr.
-# fpts.offense.week6$Name <- sub(' Jr.', '', fpts.offense.week6$Name) # remove Jr.
 
 ####### COMBINE ALL WEEKS #########
-fpts.offense.allweeks <- as.data.frame(fpts.offense.week2$Name)
+fpts.offense.allweeks <- as.data.frame(fpts.offense.week2$Name) # week 2 has most players (?)
 colnames(fpts.offense.allweeks) <- c('Name')
+
 fpts.offense.allweeks$wk1_dfn_error <- fpts.offense.week1$Error_fpts_dfn[match(fpts.offense.allweeks$Name, fpts.offense.week1$Name)]
 fpts.offense.allweeks$wk2_dfn_error <- fpts.offense.week2$Error_fpts_dfn[match(fpts.offense.allweeks$Name, fpts.offense.week2$Name)]
 fpts.offense.allweeks$wk3_dfn_error <- fpts.offense.week3$Error_fpts_dfn[match(fpts.offense.allweeks$Name, fpts.offense.week3$Name)]
 fpts.offense.allweeks$wk4_dfn_error <- fpts.offense.week4$Error_fpts_dfn[match(fpts.offense.allweeks$Name, fpts.offense.week4$Name)]
 fpts.offense.allweeks$wk5_dfn_error <- fpts.offense.week5$Error_fpts_dfn[match(fpts.offense.allweeks$Name, fpts.offense.week5$Name)]
-#fpts.offense.allweeks$wk6_dfn_error <- fpts.offense.week6$Error_fpts_dfn[match(fpts.offense.allweeks$Name, fpts.offense.week6$Name)]
+fpts.offense.allweeks$wk6_dfn_error <- fpts.offense.week6$Error_fpts_dfn[match(fpts.offense.allweeks$Name, fpts.offense.week6$Name)]
+#fpts.offense.allweeks$wk7_dfn_error <- fpts.offense.week7$Error_fpts_dfn[match(fpts.offense.allweeks$Name, fpts.offense.week7$Name)]
 
 # compute standard deviation
 fpts.offense.allweeks <- transform(fpts.offense.allweeks, sd_dfn_error=apply(fpts.offense.allweeks,1, sd, na.rm = TRUE))
