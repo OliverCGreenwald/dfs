6e1b3e2b30d9773bc2dd6af85fc9d78aa7916eb3
Alan <alan.du20@gmail.com>
2016-11-01 17:28:55 -0400

    update

--- optimizationCode/scrapeTargets.R
+++ optimizationCode/scrapeTargets.R
@@ -10,26 +10,48 @@ library('rvest')
 
 ####### SET YEAR, WEEK, POSITION #########
 yr <- '2016'
-wk <- ''
+week.latest <- ceiling((as.numeric(Sys.Date()) - as.numeric(as.Date("2016-09-11")))/7 + 1) - 1
 pos <- ''
 
 ####### SET TEAM NAMES FOLLOWING NFL SAVANT NAMING CONVENTION #########
-team.names <- c('ARI','ATL','BAL','BUF','CAR','CHI','CIN','CLE','DAL','DEN','DET','GB','HOU','IND','JAX','KC','MIA','MIN','NE','NO','NYG','NYJ','OAK','PHI','PIT','SD','SEA','SF','LA','TB','TEN','WAS')
+team.names <- c('ARI','ATL','BAL','BUF','CAR','CHI','CIN','CLE','DAL','DEN','DET','GB','HOU','IND','JAX','KC',
+                'MIA','MIN','NE','NO','NYG','NYJ','OAK','PHI','PIT','SD','SEA','SF','LA','TB','TEN','WAS')
 
 ####### SCRAPE HTML TABLE FOR ALL TEAMS #########
-targets.df <- data.frame(matrix(data = NA, nrow = 0, ncol = 9))
-colnames(targets.df) <- c('Rank', 'Name', 'Team', 'Pos', 'Completions', 'Targets', 'Comp.Pct', 'TDs', 'Target.Pct')
-for (i in 1:length(team.names)) {
-  url <- paste0("http://nflsavant.com/targets.php?ddlTeam=", team.names[i], "&ddlYear=", yr, "&week=", wk, "&rz=all&ddlPosition=", pos)
-  temp.df <- url %>%
-    read_html() %>%
-    html_nodes(xpath='//*[@id="tblTargetsTotal"]') %>%
-    html_table()
-  temp.df <- temp.df[[1]]
-  targets.df <- rbind(targets.df, temp.df)
+for (i in 1:week.latest) {
+  targets.df <- data.frame(matrix(data = NA, nrow = 0, ncol = 9)) # initialize df to store targets data
+  #colnames(targets.df) <- c('Rank', 'Name', 'Team', 'Pos', 'Completions', 'Targets', 'Comp.Pct', 'TDs', 'Target.Pct')
+  
+  for (j in 1:length(team.names)) {
+    url <- paste0("http://nflsavant.com/targets.php?ddlTeam=", team.names[j], "&ddlYear=", yr, "&week=", i, "&rz=all&ddlPosition=", pos)
+    temp.df <- url %>%
+      read_html() %>%
+      html_nodes(xpath='//*[@id="tblTargetsTotal"]') %>%
+      html_table()
+    temp.df <- temp.df[[1]]
+    targets.df <- rbind(targets.df, temp.df)
+  }
+  
+  targets.df$Week.Num <- i
+  name <- paste("targets.df.wk", i, sep = "")
+  assign(name, targets.df)
+  
+  # aggregate (we want rolling stats)
+  if (i == 1) break
+  
+  #fpts.realized.week4$DfnPreds <- fpts.dfn.offense.week4$Proj.FP[match(fpts.realized.week4$Player, fpts.dfn.offense.week4$Player.Name)]
 }
 
-head(targets.df)
+####### SET TEAM-LEVEL RANKING FOR EACH PLAYER #########
+for (j in week.latest:1) {
+  print(j)
+  if (j == 6) break
+}
+
+
+
+
+
 
 
 
