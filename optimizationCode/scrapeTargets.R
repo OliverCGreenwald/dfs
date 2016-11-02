9b49a13938277a3bef03d905a18ef63dc3c953bb
Alan <alan.du20@gmail.com>
2016-11-02 00:02:14 -0400

    update targets code

--- optimizationCode/scrapeTargets.R
+++ optimizationCode/scrapeTargets.R
@@ -22,6 +22,7 @@ for (i in 1:week.latest) {
   targets.df <- data.frame(matrix(data = NA, nrow = 0, ncol = 9)) # initialize df to store targets data
   #colnames(targets.df) <- c('Rank', 'Name', 'Team', 'Pos', 'Completions', 'Targets', 'Comp.Pct', 'TDs', 'Target.Pct')
   
+  # iterate through all teams
   for (j in 1:length(team.names)) {
     url <- paste0("http://nflsavant.com/targets.php?ddlTeam=", team.names[j], "&ddlYear=", yr, "&week=", i, "&rz=all&ddlPosition=", pos)
     temp.df <- url %>%
@@ -32,23 +33,39 @@ for (i in 1:week.latest) {
     targets.df <- rbind(targets.df, temp.df)
   }
   
+  # add week number
   targets.df$Week.Num <- i
+  
+  # aggregate (we want rolling stats, so add on previous week's data)
+  if (i != 1) {
+    # targets.df.prev <- eval(parse(text=paste("targets.df.wk", i-1, sep = ""))) # previous week's df
+    # targets.df$Completions[match(targets.df$Name, targets.df.prev$Name)] <- targets.df$Completions + targets.df.prev$Completions # add on previous week's completions
+    # targets.df$Targets[match(targets.df$Name, targets.df.prev$Name)] <- targets.df$Targets + targets.df.prev$Targets # add on previous week's targets
+    # targets.df$TDs[match(targets.df$Name, targets.df.prev$Name)] <- targets.df$TDs + targets.df.prev$TDs # add on previous week's TDs
+  }
+  
+  # assign variable name (with week number) to targets.df
   name <- paste("targets.df.wk", i, sep = "")
   assign(name, targets.df)
   
-  # aggregate (we want rolling stats)
-  if (i == 1) break
-  
-  #fpts.realized.week4$DfnPreds <- fpts.dfn.offense.week4$Proj.FP[match(fpts.realized.week4$Player, fpts.dfn.offense.week4$Player.Name)]
+  print(i)
 }
 
 ####### SET TEAM-LEVEL RANKING FOR EACH PLAYER #########
-for (j in week.latest:1) {
-  print(j)
-  if (j == 6) break
+# i <- 2
+# targets.df.prev <- targets.df.wk1
+# targets.df <- targets.df.wk2
+# ind.match <- which(targets.df$Name %in% targets.df.prev$Name)
+# match(targets.df$Name, targets.df.prev$Name)
+# targets.df$Completions[ind.match] <- targets.df$Completions[ind.match] + targets.df.prev$Completions[targets.df.prev$Name==targets.df$Name] # add on previous week's completions
+# 
+# targets.df$Completions <- targets.df$Completions + targets.df.prev$Completions[match]
+# projections.data$Actual_fpts <- realized.data$Actual.Score[match(projections.data$Name, realized.data$Player)]
+
+####### WRITE TO FILE #########
+for (i in 1:week.latest) {
+  
 }
-
-
 
 
 
