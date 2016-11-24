4ea9cca8a58857520e6e156790ef5467d3eccbaf
Alan <alan.du20@gmail.com>
2016-11-24 16:29:52 -0500

    i'm thankful for clean code

--- optimizationCode/dataCleaning.R
+++ optimizationCode/dataCleaning.R
@@ -41,25 +41,17 @@ for (i in 1:week.latest) {
 
 
 ####### REMOVE FIRST AND LAST NAME FROM CLEANED INPUT OFFENSE CSV #########
-fpts.offense.week1 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk1/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week2 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk2/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week3 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk3/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week4 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk4/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week5 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk5/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week6 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk6/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week7 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk7/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week8 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk8/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week9 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk9/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week10 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk10/offensive_players.csv', stringsAsFactors = F)
-fpts.offense.week11 <- read.csv(file = 'optimizationCode/data_warehouse/2016_cleaned_input/wk11/offensive_players.csv', stringsAsFactors = F)
-
-fpts.offense.allwks <- list(fpts.offense.week1,fpts.offense.week2,fpts.offense.week3,fpts.offense.week4,fpts.offense.week5,fpts.offense.week6,fpts.offense.week7,
-                            fpts.offense.week8,fpts.offense.week9,fpts.offense.week10,fpts.offense.week11)
-
-for (i in 1:length(fpts.offense.allwks)) {
-  temp <- fpts.offense.allwks[[i]]
+week.latest <- ceiling((as.numeric(Sys.Date()) - as.numeric(as.Date("2016-09-11")))/7 + 1) - 1
+for (i in 1:week.latest) {
+  # load files
+  name <- paste("cleaned_input_offense_wk", i, sep = "")
+  assign(name, read.csv(file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/wk', i, '/offensive_players.csv'), stringsAsFactors = F))
+  
+  # remove Sr. and Jr.
+  temp <- eval(parse(text=name))
   temp$Name <- sub(' Sr.', '', temp$Name)
   temp$Name <- sub(' Jr.', '', temp$Name)
-  fpts.offense.allwks[[i]] <- temp
-  write.csv(fpts.offense.allwks[[i]], file = paste0("optimizationCode/data_warehouse/2016_cleaned_input/wk", i, "/offensive_players.csv"), row.names = F)
+  
+  # write to file
+  write.csv(temp, file = paste0("optimizationCode/data_warehouse/2016_cleaned_input/wk", i, "/offensive_players.csv"), row.names = F)
 }
--- resultsAnalysis/dataCleaning.R
+++ resultsAnalysis/dataCleaning.R
@@ -6,25 +6,17 @@
 # TODO: clean up this hard coded shit
 
 ####### REMOVE FIRST AND LAST NAME FROM PLAYER PRODUCTION CSV #########
-fpts.realized.week1 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week1.csv', stringsAsFactors = F)
-fpts.realized.week2 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week2.csv', stringsAsFactors = F)
-fpts.realized.week3 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week3.csv', stringsAsFactors = F)
-fpts.realized.week4 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week4.csv', stringsAsFactors = F)
-fpts.realized.week5 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week5.csv', stringsAsFactors = F)
-fpts.realized.week6 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week6.csv', stringsAsFactors = F)
-fpts.realized.week7 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week7.csv', stringsAsFactors = F)
-fpts.realized.week8 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week8.csv', stringsAsFactors = F)
-fpts.realized.week9 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week9.csv', stringsAsFactors = F)
-fpts.realized.week10 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week10.csv', stringsAsFactors = F)
-fpts.realized.week11 <- read.csv(file = 'resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week11.csv', stringsAsFactors = F)
-
-fpts.realized.allwks <- list(fpts.realized.week1,fpts.realized.week2,fpts.realized.week3,fpts.realized.week4,fpts.realized.week5,fpts.realized.week6,fpts.realized.week7,
-                             fpts.realized.week8,fpts.realized.week9,fpts.realized.week10,fpts.realized.week11)
-
-for (i in 1:length(fpts.realized.allwks)) {
-  temp <- fpts.realized.allwks[[i]]
+week.latest <- ceiling((as.numeric(Sys.Date()) - as.numeric(as.Date("2016-09-11")))/7 + 1) - 1
+for (i in 1:week.latest) {
+  # load files
+  name <- paste("draftkings_player_production_week", i, sep = "")
+  assign(name, read.csv(file = paste0('resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week', i, '.csv'), stringsAsFactors = F))
+  
+  # remove Sr. and Jr.
+  temp <- eval(parse(text=name))
   temp$Player <- sub(' Sr.', '', temp$Player)
   temp$Player <- sub(' Jr.', '', temp$Player)
-  fpts.realized.allwks[[i]] <- temp
-  write.csv(fpts.realized.allwks[[i]], file = paste0("resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week", i, ".csv"), row.names = F)
+  
+  # write to file
+  write.csv(temp, file = paste0("resultsAnalysis/data_warehouse/player_weekly_performance/draftkings_player_production_week", i, ".csv"), row.names = F)
 }
