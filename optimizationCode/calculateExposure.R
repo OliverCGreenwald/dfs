5be0ecdbe134c234499e471681ad17bb56c3a7f4
Alan <alan.du20@gmail.com>
2016-12-18 12:52:07 -0500

    week 15

--- optimizationCode/calculateExposure.R
+++ optimizationCode/calculateExposure.R
@@ -1,7 +1,7 @@
 #setwd("~/Projects/DFS/")
 #setwd("~/Documents/PrincetonFall16/fantasyfootball/DFS/")
 
-data <- read.csv("optimizationCode/output2.csv", stringsAsFactors = F, header = T)
+data <- read.csv("optimizationCode/output.csv", stringsAsFactors = F, header = T)
 # data <- read.csv("optimizationCode/submitted_lineups/week6_$3_lineups.csv")
 
 occurences<-sort(table(unlist(data)), decreasing=T)
