6f67b748f9e7e02dc96bba994951899bab08882a
Alan <alan.du20@gmail.com>
2016-12-21 20:39:28 -0500

    removing adding floor, ceiling, and actual

--- projectionsAnalysis/regression.R
+++ projectionsAnalysis/regression.R
@@ -4,7 +4,6 @@
 ####### DESCRIPTION #########
 # In this file we run a regression on Rotogrinders and Daily Fantasy Nerd projections to output combined predictions.
 # Filtered out players projected to get 0 fpts. Offense only.
-# We also add floor, ceiling, and actual fpts to the 2016_cleaned_input (offense and defense) and 2016_cleaned_input/all_data (just offense) folders.
 
 ####### LOAD DFN FILES #########
 week.latest <- ceiling((as.numeric(Sys.Date()) - as.numeric(as.Date("2016-09-11")))/7 + 1) - 1
@@ -21,58 +20,7 @@ for (i in 1:(week.latest+1)) {
   temp.df <- eval(parse(text=name))
   temp.df$Week.Num <- i
   assign(name, temp.df)
-  
-  #--- defense (only used for appending Actual to 2016_cleaned_input) ---#
-  name <- paste("dfn_defense_week", i, sep = "")
-  if (i == (week.latest+1)) {
-    assign(name, read.csv(file = paste0('optimizationCode/data_warehouse/dailyfantasynerd/dfn_deffense_week', i, '.csv'), stringsAsFactors = F))
-  } else {
-    assign(name, read.csv(file = paste0('optimizationCode/data_warehouse/dailyfantasynerd/updates/dfn_defense_week', i, '.csv'), stringsAsFactors = F))
-  }
 }
-
-
-####### ADD FLOOR, CEIL, ACTUAL (OFF AND DEF) TO 2016_CLEANED_INPUT/ALL_DATA FILES (only run after current week's data is prepared) #########
-# for (i in 2:week.latest) {
-  i <- week.latest + 1
-  
-  #--- 2016_cleaned_input/all_data ---#
-  temp <- read.csv(file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/all_data/wk', i, '/offensive_players.csv'), stringsAsFactors = F)
-  temp.dfn <- eval(parse(text=paste0("dfn_offense_week", i)))
-  
-  # add Projection_dfn_floor, Projection_dfn_ceiling, Actual
-  temp$Projection_dfn_floor <- temp.dfn$Floor.FP[match(paste0(temp$Name,temp$Position), paste0(temp.dfn$Player.Name,temp.dfn$Pos))]
-  temp$Projection_dfn_ceiling <- temp.dfn$Ceil.FP[match(paste0(temp$Name,temp$Position), paste0(temp.dfn$Player.Name,temp.dfn$Pos))]
-  temp$Actual <- temp.dfn$Actual.FP[match(paste0(temp$Name,temp$Position), paste0(temp.dfn$Player.Name,temp.dfn$Pos))]
-  
-  # write to file
-  write.csv(temp, file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/all_data/wk', i, '/offensive_players.csv'), row.names = F)
-  
-  #--- 2016_cleaned_input (offense) ---#
-  temp <- read.csv(file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/wk', i, '/offensive_players.csv'), stringsAsFactors = F)
-  temp.dfn <- eval(parse(text=paste0("dfn_offense_week", i)))
-  
-  # add Actual
-  temp$Actual <- temp.dfn$Actual.FP[match(paste0(temp$Name,temp$Position), paste0(temp.dfn$Player.Name,temp.dfn$Pos))]
-  temp$Actual[is.na(temp$Actual)==T] <- 0 # replace NA's with 0's b/c can't run julia with NA's
-  
-  # write to file
-  write.csv(temp, file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/wk', i, '/offensive_players.csv'), row.names = F)
-  
-  #--- 2016_cleaned_input (defense) ---#
-  temp <- read.csv(file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/wk', i, '/defenses.csv'), stringsAsFactors = F)
-  temp.dfn <- eval(parse(text=paste0("dfn_defense_week", i)))
-  
-  # name cleaning
-  temp$Team <- toupper(temp$Team)
-  
-  # add Actual
-  temp$Actual <- temp.dfn$Actual.FP[match(temp$Team, temp.dfn$Team)]
-  # temp$Actual[is.na(temp$Actual)==T] <- 0 # replace NA's with 0's b/c can't run julia with NA's (catch NAs)
-  
-  # write to file
-  write.csv(temp, file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/wk', i, '/defenses.csv'), row.names = F)
-# }
 
 
 ####### AGGREGATE DATA #########
@@ -154,7 +102,7 @@ for (i in 1:week.latest) {
 
 ####### ADD REGRESSED PREDICTIONS TO 2016_CLEANED_INPUT FILES #########
 # for (i in 2:week.latest) { # change to week.latest+1 once current week's data has been scraped
-  i <- week.latest + 1
+  i <- week.latest+1
   
   temp <- read.csv(file = paste0('optimizationCode/data_warehouse/2016_cleaned_input/wk', i,'/offensive_players.csv'), stringsAsFactors = F)
   
