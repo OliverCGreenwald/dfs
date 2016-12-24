298547ed8957798ab778b4e3601b7f0879ebc577
Alan <alan.du20@gmail.com>
2016-12-23 21:48:31 -0500

    week 16 prep

--- optimizationCode/calculateExposure.R
+++ optimizationCode/calculateExposure.R
@@ -1,10 +1,54 @@
 #setwd("~/Projects/DFS/")
 #setwd("~/Documents/PrincetonFall16/fantasyfootball/DFS/")
 
-data <- read.csv("optimizationCode/output.csv", stringsAsFactors = F, header = T)
-# data <- read.csv("optimizationCode/submitted_lineups/week6_$3_lineups.csv")
+output.data <- read.csv("optimizationCode/output.csv", stringsAsFactors = F, header = T)
 
-occurences<-sort(table(unlist(data)), decreasing=T)
+# all players
+occurences <- sort(table(unlist(output.data)), decreasing=T)
+exposure<- occurences / nrow(output.data)
+exposure
 
-exposure<- occurences / nrow(data)
+# QB
+occurences <- sort(table(unlist(output.data$QB)), decreasing=T)
+exposure <- occurences / nrow(output.data)
+exposure
+
+# TE
+occurences <- sort(table(unlist(output.data$TE)), decreasing=T)
+exposure <- occurences / nrow(output.data)
+exposure
+
+# DST
+occurences <- sort(table(unlist(output.data$DST)), decreasing=T)
+exposure <- occurences / nrow(output.data)
+exposure
+
+# RB1 and RB2
+occurences <- sort(table(unlist(output.data[,c("RB","RB.1")])), decreasing=T)
+exposure<- occurences / nrow(output.data)
+exposure
+
+# WR1, WR2, WR3
+occurences <- sort(table(unlist(output.data[,c("WR","WR.1","WR.2")])), decreasing=T)
+exposure <- occurences / nrow(output.data)
+exposure
+
+# WR1
+occurences <- sort(table(unlist(output.data$WR)), decreasing=T)
+exposure <- occurences / nrow(output.data)
+exposure
+
+# WR2
+occurences <- sort(table(unlist(output.data$WR.1)), decreasing=T)
+exposure <- occurences / nrow(output.data)
+exposure
+
+# WR3
+occurences <- sort(table(unlist(output.data$WR.2)), decreasing=T)
+exposure <- occurences / nrow(output.data)
+exposure
+
+# FLEX
+occurences <- sort(table(unlist(output.data$FLEX)), decreasing=T)
+exposure <- occurences / nrow(output.data)
 exposure
---
+++ optimizationCode/data_warehouse/2016_cleaned_input/wk16/includes_sun-mon/readme.txt
@@ -0,0 +1,24 @@
+Set to ceiling:
+Malcolm Mitchell (5200)
+Amari Cooper (6500)
+Tyrell Williams (5800)
+
+
+Set to (ceiling-proj)/2:
+Michael Crabtree (6100)
+Davante Adams (5400)
+Michael Thomas (6000)
+
+
+Maybe:
+Ty Montgomery (5900)
+T.Y. Hilton (7800)
+
+
+Fix:
+Allen Robinson (no Marquise Lee)
+Marvin Jones and Anquan Boldin
+
+
+How to find cheap players that might go off:
+- O/U
