10f081685abac221b0a6584824a132821b3a2f84
alandu20 <aydu@princeton.edu>
2017-04-19 00:51:18 -0400

    Covariance matrix

--- MLB/optimizationCode/create_covariance_matrix.R
+++ MLB/optimizationCode/create_covariance_matrix.R
@@ -7,10 +7,13 @@ if(file.exists("~/Projects/DFS/")) {
 
 ####### Description #######
 # Constructs the covariance matrix for MVO.
+#
+# TODO:
+# - determine if pairwise.complete.obs is appropriate for our problem. considered dangerous
 
 contest.date <- "2017-04-02"
 
-####### Get List of All Players that have Played a Game Up to Today-1 #######
+####### Aggregate All Player Data for Each Day #######
 # load contest info file
 contest_info <- read.csv(file = 'MLB/data_warehouse/contests.csv', stringsAsFactors = F)
 # dates <- seq(from = as.Date("2017-04-04"), to = as.Date("2017-04-05"), by = "day")
@@ -33,8 +36,52 @@ for (d in 1:length(dates)) {
   
   # append to the unique rows (players) to the running list
   list_all_players <- rbind(list_all_players, unique(list_players_day))
-  remove(list_players_day) # remove
+  remove(list_players_day) # remove temp
 }
 
+####### Construct Matrix of Historical Fpts and Compute Covariance Matrix #######
+# list of unique player names and their position
+temp_names <- paste0(list_all_players$Name, list_all_players$teamAbbrev)
+names_unique_players <- unique(temp_names)
+
+# initialize historical fpts matrix
+hist_fpts_mat <- as.data.frame(matrix(data = NA, nrow = length(names_unique_players), ncol = length(dates)))
+colnames(hist_fpts_mat) <- dates
+rownames(hist_fpts_mat) <- names_unique_players
+
+# fill historical fpts matrix
+for (i in 1:nrow(hist_fpts_mat)) {
+  for (j in 1:ncol(hist_fpts_mat)) {
+    temp <- list_all_players$Actual_fpts[paste0(list_all_players$Name, list_all_players$teamAbbrev)==rownames(hist_fpts_mat)[i] & list_all_players$Date==colnames(hist_fpts_mat)[j]]
+    if (length(temp)==0) {
+      hist_fpts_mat[i,j] <- NA
+    } else {
+      hist_fpts_mat[i,j] <- temp
+    }
+  }
+}
+# temp <- hist_fpts_mat
+# remove rows with NA count > round(length(dates)/2)
+inds.remove <- NULL
+for (i in 1:nrow(hist_fpts_mat)) {
+  if (sum(is.na(hist_fpts_mat[i,])) > round(length(dates)/2)) {
+    inds.remove <- c(inds.remove, i)
+  }
+}
+hist_fpts_mat <- hist_fpts_mat[-inds.remove,]
+
+
+# construct covariance matrix
+cov_mat <- cov(t(hist_fpts_mat), use = "pairwise.complete.obs") # complete.obs
+View(cov_mat)
+
+# indicies of max covariance
+which(cov_mat == max(cov_mat, na.rm = T), arr.ind = TRUE)
+
+# find the top n largest values (decreasing order)
+n <- 25
+x <- which(cov_mat >= sort(cov_mat, decreasing = T)[n], arr.ind = T)
+x.order <- order(cov_mat[x], decreasing = T) # decreasing order
+x[x.order,]
 
 
