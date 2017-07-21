39354aac40174f8f707689bbfbc92924e37689a9
alandu20 <aydu@princeton.edu>
2017-07-21 11:52:03 -0400

    Rest of form 5 without covariance

--- MLB/resultsAnalysis/testingSuite_baseline.R
+++ MLB/resultsAnalysis/testingSuite_baseline.R
@@ -85,11 +85,14 @@ ggplot(data=plotdata,
 
 
 # view a particular formulation
+# form_name <- "formulations.formulation5_covar_stacksize_5_overlap_5_lineups_150_lambda_0.001_exposure_P0.8_exposure_B10.3_exposure_B20.4_exposure_B30.6_exposure_C0.3_exposure_SS0.5_exposure_OF0.6_covar_chg75p_exp(spike).csv"
 form_name <- "formulations.formulation5_covar_stacksize_5_overlap_5_lineups_150_lambda_0.002_exposure_P0.8_exposure_B10.3_exposure_B20.4_exposure_B30.6_exposure_C0.3_exposure_SS0.5_exposure_OF0.6_covar_chg75p_exp(spike).csv"
 # form_name <- "formulations.formulation5_covar_stacksize_5_overlap_5_lineups_150_lambda_0.002_exposure_P0.8_exposure_B10.5_exposure_B20.4_exposure_B30.6_exposure_C0.5_exposure_SS0.5_exposure_OF0.6_covar_chg75p_exp(spike)"
 # form_name <- "formulations.formulation3_covar_stacksize_5_overlap_5_lineups_150_lambda_0.002_exposure_0.6_covar_chg75p_exp(spike)"
 inds_form <- which(PnL$Name==form_name)
 plot(as.Date(PnL$Date[inds_form]), PnL$PnL[inds_form], type = "b")
+PnL$PnL[inds_form]
+sum(PnL$PnL[inds_form])
 mean(PnL$PnL[PnL$PnL<0])
 
 # write to file
