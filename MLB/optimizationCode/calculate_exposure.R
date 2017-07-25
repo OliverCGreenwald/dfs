549748d66fa12e234f0fa3ad0b8fd4265a0dff07
Michael Chiang <mlchiang@princeton.edu>
2017-07-25 01:00:25 -0400

    add form7 stuff

--- MLB/optimizationCode/calculate_exposure.R
+++ MLB/optimizationCode/calculate_exposure.R
@@ -8,10 +8,10 @@ if(file.exists("~/Projects/DFS/")) {
 ####### Description #######
 # Calculates exposure to each player for a given set of lineups.
 
-contest_date <- "2017-04-21"
-entry_fee <- "$33.00"
-contest_name <- "MLB$300KFastball"
-lineup_file <- "formulations.formulation1_covar_stacksize_5_overlap_6_lineups_150_lambda_0.001_exposure_0.4"
+contest_date <- "2017-07-07"
+entry_fee <- "$44.00"
+contest_name <- "MLB$350KMediumAllStarBreakSpecial"
+lineup_file <- "formulations.formulation7_covar_stacksize_5_overlap_5_lineups_150_lambda_0.0_exposure_P0.8_exposure_B10.3_exposure_B20.4_exposure_B30.6_exposure_C0.3_exposure_SS0.5_exposure_OF0.6_min_pitcher_exposure0.6_covar_chg75p_exp(spike)"
 lineup.data <- read.csv(file = paste0("MLB/data_warehouse/", contest_date,"/" , paste0(entry_fee,"entry_",gsub(" ", "", contest_name)), "/lineups/", lineup_file, ".csv"), stringsAsFactors = F, header = T)
 
 # All players
