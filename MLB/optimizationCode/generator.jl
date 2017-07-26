e73c8214318282ad7baccdb8abd02d556485e914
alandu20 <aydu@princeton.edu>
2017-07-26 19:00:01 -0400

    Form 9 (form 5 with C salary <= 3200)

--- MLB/optimizationCode/generator.jl
+++ MLB/optimizationCode/generator.jl
@@ -24,7 +24,7 @@ num_overlap = 5;
 stack_size = 5; 
 
 #FORMULATION:  formulation is the type of formulation that you would like to use. 
-formulation = formulations.formulation5_covar
+formulation = formulations.formulation9_covar
 
 # Covariance term 
 lambda_var = 0.001
@@ -40,34 +40,26 @@ exposure_C = 0.3
 exposure_SS = 0.3
 exposure_OF = 0.6
 
+min_pitcher_exposure = 0.4
+
 ################################################################################################################
+
 contest_directory_path = string("../data_warehouse/", contest_date, "/", contest_name, "/");
 
 #path to the csv file with the players information (pitchers and hitters);
 path_pitchers = string(contest_directory_path, "pitchers.csv"); 
 path_hitters = string(contest_directory_path, "hitters.csv"); 
-path_covar_matrix = string(contest_directory_path, "covariance_mat_chg75p_exp(spike).csv");
+path_covar_matrix = string(contest_directory_path, "covariance_mat_chg75p_exp(spike).csv"); 
 # path_to_output is a string  that gives the path to the csv file that will give the outputted results
+
 path_to_output= string(contest_directory_path, "lineups/",
-                       string(formulation), "_stacksize_", stack_size,"_overlap_", num_overlap,"_lineups_", num_lineups,"_lambda_", lambda_var,
-                       "_exposure_P", exposure_P,"_exposure_B1", exposure_B1,"_exposure_B2", exposure_B2,"_exposure_B3", exposure_B3,"_exposure_C", exposure_C,"_exposure_SS", exposure_SS,"_exposure_OF", exposure_OF,"_covar_chg75p_exp(spike).csv");
+     string(formulation), "_stacksize_", stack_size,"_overlap_", num_overlap,"_lineups_", num_lineups,"_lambda_", lambda_var,
+     "_exposure_P", exposure_P,"_exposure_B1", exposure_B1,"_exposure_B2", exposure_B2,"_exposure_B3", exposure_B3,"_exposure_C", exposure_C,"_exposure_SS", exposure_SS,"_exposure_OF", exposure_OF,"_min_pitcher_exposure", min_pitcher_exposure,"_debug.csv"); 
 
-# path_to_output_proj is a string  that gives the path to the csv file that will give the outputted results with projected lineup points
-#path_to_output_proj = string("proj_baseball_", string(formulation), "_stacksize_", stack_size,"_overlap_", num_overlap,"_lineups_", num_lineups,".csv"); 
-
-# path_to_output_actual is a string  that gives the path to the csv file that will give the outputted results with projected and actual lineup points
-#path_to_output_actual = string("actual_baseball_", string(formulation), "_stacksize_", stack_size,"_overlap_", num_overlap,"_lineups_", num_lineups,".csv"); 
-
-
-#########################################################################
-# Running the code
-
-println("Calculating DraftKings baseball linueps.\n ", num_lineups, " lineups\n","Formulation  ",formulation,
-"\nOverlap = ", num_overlap,"\nStack size = ", stack_size)
 
 start_time = time_ns()
 
-data_cleaning.create_lineups(num_lineups, num_overlap, stack_size,formulation, path_pitchers,path_hitters, path_covar_matrix, lambda_var, exposure, exposure_P,exposure_B1,exposure_B2,exposure_B3,exposure_C,exposure_SS,exposure_OF, path_to_output);
+data_cleaning.create_lineups(num_lineups, num_overlap, stack_size,formulation, path_pitchers,path_hitters, path_covar_matrix, lambda_var, exposure, exposure_P,exposure_B1,exposure_B2,exposure_B3,exposure_C,exposure_SS,exposure_OF, min_pitcher_exposure, path_to_output);
 
 
 println("##############################")
